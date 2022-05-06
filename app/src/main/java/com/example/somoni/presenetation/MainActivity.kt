82da0cc1ce971acac583b25cf32fed8d8895f069
Ismoil <ismoilhukumatov9990@gmail.com>
2022-05-06 14:38:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/somoni/presenetation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.somoni.presenetation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.somoni.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
